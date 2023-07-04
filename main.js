85e694c6a7523d78607f459a0ce4e758cfa328d4
TheSharkFullCode <agedjoscar@hotmail.com>
2023-07-04 09:50:08 +0200

    done

---
+++ main.js
@@ -0,0 +1,52 @@
+// alert("hello world");
+// Calcula la operación 2 elevado a la 4.
+function potencia(num,dta){
+    dta = num**4;
+    console.log(dta);
+}
+potencia(2);
+// ensallo
+// let pot1 = 5;
+// let pot2 = pot1**4
+// console.log(pot2);
+
+// Calcula la raíz cuadrada de 25.
+
+const numero = 25;
+ const raizcuadrada = Math.sqrt(numero);
+ console.log(raizcuadrada);
+
+//no entiendo porque aveces no me devuelve su valor, si por ejemplo lo hago con retunr: + console.log. y lo habia hecho atraves de una funcion.
+
+
+// Calcula el valor absoluto de la operación 4 - 5.
+
+let operation = Math.abs(4-5);
+console.log(operation);
+
+// Calcula la raíz cuadrada de 2 y redondea el resultado.
+let rCdrada = Math.sqrt(2);
+console.log(rCdrada);
+console.log(Math.round(rCdrada));
+// let redonded = Math.round(rCdrada);
+// console.log(redonded);
+
+// Calcula la operación 10 dividido entre 3 y redondea el resultado hacia abajo.
+const numbers = 10;
+let result = numbers / 3; 
+console.log(result);
+console.log(Math.floor(result));
+
+
+// Calcula la operación 3 dividido entre 2 y redondea el resultado hacia arriba.
+let result2 =3;
+result2 = 3 / 2;
+console.log(Math.ceil(result2));
+// Dada la siguiente lista de números (5, 3, 6, 4, 2, 9, 8):
+// Encuentra el número más bajo y guárdalo en la variable min.
+let numberMin = (5, 3, 6, 4, 2, 9, 8);
+console.log(Math.min(5, 3, 6, 4, 2, 9, 8));
+// Encuentra el número más alto y guárdalo en la variable max.
+console.log(Math.max(5, 3, 6, 4, 2, 9, 8));
+
+console.log(`----------------------------------------`);
